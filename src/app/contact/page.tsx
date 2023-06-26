7383d4fb500690a5ccf592459de145cd5e34b010
Josh Harris <49429928+jharris711@users.noreply.github.com>
2023-06-26 15:33:29 -0400

    Change tagline

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -8,8 +8,8 @@ const ContactPage = () => {
           Contact Us
         </h2>
         <p className='mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl'>
-          Got a technical issue? Want to send feedback about a beta feature?
-          Need details about our Business plan? Let us know.
+          Connect with us and join the conversation on our contact page - we
+          love hearing from you!
         </p>
         <ContactForm />
       </div>
